d7755ba1d8bdf86cea6833d534aa9c0b123cc1da
Esclender <elesclenderlugo@gmail.com>
2024-06-21 13:20:18 -0500

    chore: no codegen needed

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import { loadEnv } from 'vite'
+
 
 export default mergeConfig(
   viteConfig,
@@ -8,7 +10,8 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      env: loadEnv('', process.cwd(), ''),
     }
   })
 )
